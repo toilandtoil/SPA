a5df0a9db101be075d2e5fe45d65785f3b8fba3b
michael_buckley <michael@toilandtoil.com>
2014-04-15 17:25:22 -0500

    1st crack at Listing 4.15

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -3,16 +3,41 @@
  * Chat feature module for SPA
  */
 
-/*global $, spa */
+/*global $, spa, getComputedStyle */
 spa.chat = (function () {
     //---------------- BEGIN MODULE SCOPE VARIABLES --------------
     var
     configMap = {
-        main_html: String() + '<div style="padding:1em; color:#fff;">' + 'You can do this, Buckley' + '</div>',
-        settable_map: {}
+        main_html: String() + '<div class="spa-chat">' + '<div class="spa-chat-head">' + '<div class="spa-chat-head-toggle">+</div>' + '<div class="spa-chat-head-title">' + 'Chat' + '</div>' + '</div>' + '<div class="spa-chat-closer">x</div>' + '<div class="spa-chat-sizer">' + '<div class="spa-chat-msgs"></div>' + '<div class="spa-chat-box">' + '<input type="text"/>' + '<div>send</div>' + '</div>' + '</div>' + '</div>',
+
+        settable_map: {
+            slider_open_time: true,
+            slider_close_time: true,
+            slider_opened_em: true,
+            slider_closed_em: true,
+            slider_opened_title: true,
+            slider_closed_title: true,
+            chat_model: true,
+            people_model: true,
+            set_chat_anchor: true
+        },
+        slider_open_time: 250,
+        slider_close_time: 250,
+        slider_opened_em: 16,
+        slider_closed_em: 2,
+        slider_opened_title: 'Click to close',
+        slider_closed_title: 'Click to open',
+        chat_model: null,
+        people_model: null,
+        set_chat_anchor: null
     },
         stateMap = {
-            $container: null
+            $append_target: null,
+            position_type: 'closed',
+            px_per_em: 0,
+            slider_hidden_px: 0,
+            slider_closed_px: 0,
+            slider_opened_px: 0
         },
         jqueryMap = {},
         setJqueryMap, configModule, initModule;
