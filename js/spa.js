0d8a8fc0a7deeed17ee684f5fc2ea321b3d17105
michael_buckley <michael@toilandtoil.com>
2014-04-17 16:35:51 -0500

    Chapter 5, through pg163, listing 5.13

--- js/spa.js
+++ js/spa.js
@@ -5,7 +5,9 @@
 
 /*global $, spa */
 var spa = (function () {
+    'use strict';
     var initModule = function ( $container ) {
+        spa.model.initModule();
         spa.shell.initModule( $container );
     };
     return { initModule: initModule };
