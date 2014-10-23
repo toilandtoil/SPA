f90259022109e0d3b672dc9e2647d6d43507d282
Michael Buckley <mbuckl1@hallmark.com>
2014-10-23 17:25:53 -0500

    Finished with chapter 7

--- webapp/socket.js
+++ webapp/socket.js
@@ -1,29 +1,51 @@
 /*
- * socket.js - simple socket.io example
+ * socket.js - dynamic JS loading example
  */
 
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
-    countUp,
+    setWatch,
     http = require( 'http' ),
     express = require( 'express' ),
     socketIo = require( 'socket.io' ),
+    fsHandle = require( 'fs' ),
     app = express(),
     server = http.createServer( app ),
     io = socketIo.listen( server ),
-    countIdx = 0
+    watchMap = {}
     ;
 // ------------- END MODULE SCOPE VARIABLES ---------------
 // --------------- BEGIN UTILITY METHODS ------------------
-countUp = function () {
-    countIdx++;
-    console.log( countIdx );
-    io.sockets.send( countIdx );
+setWatch = function ( url_path, file_type ) {
+    console.log( 'setWatch called on ' + url_path );
+    if ( ! watchMap[ url_path ] ) {
+        console.log( 'setting watch on ' + url_path );
+        fsHandle.watchFile(
+            url_path.slice(1),
+            function ( current, previous ) {
+                console.log( 'file accessed' );
+                if ( current.mtime !== previous.mtime ) {
+                    console.log( 'file changed' );
+                    io.sockets.emit( file_type, url_path );
+                }
+            }
+        );
+        watchMap[ url_path ] = true;
+    }
 };
 // ---------------- END UTILITY METHODS -------------------
 // ------------- BEGIN SERVER CONFIGURATION ---------------
 app.configure( function () {
+    app.use( function ( request, response, next ) {
+        if ( request.url.indexOf( '/js/' ) >= 0 ) {
+            setWatch( request.url, 'script' );
+        }
+        else if ( request.url.indexOf( '/css/' ) >= 0 ) {
+            setWatch( request.url, 'stylesheet' );
+        }
+        next();
+    });
     app.use( express.static( __dirname + '/' ) );
 });
 app.get( '/', function ( request, response ) {
@@ -36,5 +58,4 @@ console.log(
     'Express server listening on port %d in %s mode',
     server.address().port, app.settings.env
 );
-setInterval( countUp, 1000 );
 // ------------------ END START SERVER --------------------
