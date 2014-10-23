7828d7558e46518227587bb633a490f2962ea2c7
Michael Buckley <mbuckl1@hallmark.com>
2014-10-23 16:50:33 -0500

    Ready to start 7.4.1 Basic Authentication

--- webapp/app.js
+++ webapp/app.js
@@ -1,76 +1,37 @@
 /*
- * app.js - Express server with generic routing
+ * app.js - Express server with routes module
  */
 
 // ------------ BEGIN MODULE SCOPE VARIABLES --------------
 'use strict';
 var
-    http = require( 'http' ),
-    express = require( 'express' ),
+    http = require('http'),
+    express = require('express'),
+    routes = require('./routes'),
     app = express(),
-    server = http.createServer( app );
+    server = http.createServer(app);
 // ------------- END MODULE SCOPE VARIABLES ---------------
 // ------------- BEGIN SERVER CONFIGURATION ---------------
-app.configure( function () {
-    app.use( express.bodyParser() );
-    app.use( express.methodOverride() );
-    app.use( express.static( __dirname + '/public' ) );
-    app.use( app.router );
+app.configure(function () {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
 });
-app.configure( 'development', function () {
-    app.use( express.logger() );
-    app.use( express.errorHandler({
-        dumpExceptions : true,
-        showStack : true
-    }) );
+app.configure('development', function () {
+    app.use(express.logger());
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
 });
-app.configure( 'production', function () {
-    app.use( express.errorHandler() );
+app.configure('production', function () {
+    app.use(express.errorHandler());
 });
-
-// all configurations below are for routes
-app.get( '/', function ( request, response ) {
-    response.redirect( '/spa.html' );
-});
-
-app.all( '/:obj_type/*?', function ( request, response, next ) {
-    response.contentType( 'json' );
-    next();
-});
-
-app.get( '/:obj_type/list', function ( request, response ) {
-    response.send({ title: 'user list' });
-});
-app.post( '/:obj_type/create', function ( request, response ) {
-    response.send({ title: 'user created' });
-});
-app.get( '/:obj_type/read/:id([0-9]+)',
-    function ( request, response ) {
-        response.send({
-            title: request.params.obj_type
-                + ' with id ' + request.params.id + ' found'
-        });
-    }
-);
-app.post( '/:obj_type/update/:id([0-9]+)',
-    function ( request, response ) {
-        response.send({
-            title: request.params.obj_type
-                + ' with id ' + request.params.id + ' updated'
-        });
-    }
-);
-app.get( '/:obj_type/delete/:id([0-9]+)',
-    function ( request, response ) {
-        response.send({
-            title: request.params.obj_type
-                + ' with id ' + request.params.id + ' deleted'
-        });
-    }
-);
+routes.configRoutes(app, server);
 // -------------- END SERVER CONFIGURATION ----------------
 // ----------------- BEGIN START SERVER -------------------
-server.listen( 3000 );
+server.listen(3000);
 console.log(
     'Express server listening on port %d in %s mode',
     server.address().port, app.settings.env
